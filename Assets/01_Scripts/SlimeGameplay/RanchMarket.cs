99932d1804ca4626a65e36a38e90b6977561ab36
Samir Limachi <lls5001850@est.univalle.edu>
2026-09-20 02:21:08 -0400

    Remove all repository files

--- Assets/01_Scripts/SlimeGameplay/RanchMarket.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-namespace SlimeRancherVR {
-[RequireComponent(typeof(BoxCollider))]
-public sealed class RanchMarket : MonoBehaviour {
- public bool Sell(RanchItem item){if(!RanchGame.Instance||!item||!item.data||item.data.kind!=RanchItemKind.PinkPlort||item.Consumed)return false;int value=item.data.saleValue;if(!item.Consume())return false;RanchGame.Instance.coins+=value;RanchGame.Instance.Notify("Plort vendido: +"+value+" monedas");return true;}
- void OnTriggerEnter(Collider other){Sell(other.GetComponentInParent<RanchItem>());}
-}
-}
--- Assets/Area1/Scripts/Area1WaterSource.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-namespace SlimeRancher.Area1
-{
-    // Only colliders explicitly marked as a source can supply water.
-    [RequireComponent(typeof(Collider))]
-    public sealed class Area1WaterSource : MonoBehaviour { }
-}
